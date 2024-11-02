2c51f3061939b1740d7bf2449cbe526e8ddb9410
preotrix-prabhakar <prabhakar8859@gmail.com>
2024-11-02 22:59:18 +0530

    updates

--- src/pages/SettingsPage/Setting.jsx
+++ src/pages/SettingsPage/Setting.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import settingStyles from './setting.module.css'
+import settingStyles from './Setting.module.css'
 import Dashboard from '../../components/Dashboard/Dashboard'
 import Settings from '../../components/Setting/Setting'
 function SettingsPage() {
